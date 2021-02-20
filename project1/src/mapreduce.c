9bd665050ea91664b10ee37c318ebda1484cab25
MaxWang96 <wangzhaodong2018@gmail.com>
2021-02-19 22:57:05 -0600

    git commit -m

--- project1/src/mapreduce.c
+++ project1/src/mapreduce.c
@@ -18,26 +18,29 @@ int main(int argc, char *argv[]) {
 	// TODO: spawn mappers	
 	int i;
 	char buffer[20];
+	char buffer2[20];
 	pid_t mappers;
 	pid_t reducers;
 	for(i=0; i<nMappers; i++){
 		if((mappers = fork()) == 0){
-			execl("./mapper.c", "./mapper.c", sprintf(buffer,"%d",i), argv[1], inputFileDir, NULL);
+			sprintf(buffer,"%d",i);
+			execl("./mapper", "./mapper", buffer, argv[1], inputFileDir, NULL);
 		}else if(mappers < 0){
 			perror("Failed");
 		}
 	}
 	// TODO: wait for all children to complete execution
-	while(wait(NULL));	
+	while(wait(NULL) > 0);	
 	// TODO: spawn reducers
 	for(i=0; i<nReducers; i++){
 		if((reducers = fork()) == 0){
-			execl("./reducer.c", "./reducer.c",sprintf(buffer,"%d",i), argv[2], inputFileDir,NULL);
+			sprintf(buffer2,"%d",i);
+			execl("./reducer", "./reducer",buffer2, argv[2], inputFileDir,NULL);
 		}else if(reducers < 0){
 			perror("Failed");
 		}
 	}
 	// TODO: wait for all children to complete execution
-	while(wait(NULL));
+	while(wait(NULL) > 0);
 	return EXIT_SUCCESS;
 }
