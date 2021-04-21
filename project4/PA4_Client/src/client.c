d80843035c95313e4b34cc4c0de19c4a6b119497
Liang Zhang <liangzhang815@gmail.com>
2021-04-20 23:06:58 -0500

    interm submition

--- project4/PA4_Client/src/client.c
+++ project4/PA4_Client/src/client.c
@@ -37,9 +37,6 @@ int main(int argc, char *argv[]) {
 	strcpy(server_IP, argv[3]);
 	short server_port = atoi(argv[4]);
 	
-	printf("Setting up sock address...\n");	
-	int sockfd = socket(AF_INET , SOCK_STREAM , 0);
-	
 	struct sockaddr_in address;
 	address.sin_family = AF_INET;
 	address.sin_port = htons(server_port);
@@ -54,23 +51,30 @@ int main(int argc, char *argv[]) {
     int i;
 	for(i = 0; i < clients_num; i++){
 		if( (client = fork()) == 0 ) {
+			int sockfd = socket(AF_INET , SOCK_STREAM , 0);
 			if (connect(sockfd, (struct sockaddr *) &address, sizeof(address)) == 0) {
-				printf("connected to sever: %s\n", server_IP);
+				printf("[%d] open connection\n", i+1);
+				
+				//GET_WSTAT
 				int test[2] = {4,64};
+				printf("[%d] GET_WSTAT: %d <20 numbers>\n", i+1, test[0]);
 				write(sockfd, test, sizeof(test));
 				
 				char read_buf[200];
 				read(sockfd, read_buf, 200);
 				printf("read from server: %s\n", read_buf);
-				
-				close(sockfd);
 
+				close(sockfd); 
+				printf("[%d] close connection (successful execution)\n", i+1);
+				
+				
 			} else {
 
 				perror("Connection failed!");
-			}
+			}	
 			
-		}
+			break;		
+		} 
 	}
 
     // wait for all client processes to terminate
