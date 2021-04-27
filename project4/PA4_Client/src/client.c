276d3f0058074b37f42f07eaa53ca9afe39c9112
MaxWang96 <wangzhaodong2018@gmail.com>
2021-04-26 19:06:46 -0500

    Auto Format

--- project4/PA4_Client/src/client.c
+++ project4/PA4_Client/src/client.c
@@ -1,16 +1,17 @@
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <zconf.h>
-#include <arpa/inet.h>
-#include <ctype.h>
 #include <sys/stat.h>
-#include <dirent.h>
 #include <sys/wait.h>
+#include <zconf.h>
+
 #include "../include/protocol.h"
-#include <stdbool.h>
-#include <fcntl.h>
 
 #define NUM_ARGS 5
 
@@ -31,160 +32,157 @@ void createLogFile(void) {
         execl("/bin/rm", "rm", "-rf", "log", NULL);
 
     wait(NULL);
-    mkdir("log", ACCESSPERMS);
+    mkdir("log", 0777);
     logfp = fopen("log/log_client.txt", "w");
 }
 
-int getWordsStats(char* path, int clientID){
-	char fileName[500];
-	sprintf(fileName, "%s/%d.txt", path, clientID);
-	FILE *fp = fopen(fileName, "r");
-	if (fp == NULL) return 1;
-	char readbuf[1000];
-	while ( getLineFromFile(fp, readbuf, sizeof(readbuf)) > 0) {
-		char *token = strtok(readbuf, "\n");
-		token = strtok(token, " ");
-		while (token != NULL) {
-			wordsLength[strlen(token) - 1]++;
-			token = strtok(NULL, " ");
-		}
-	}
-	is_empty_array = 0;
-	
-	return 0;
+int getWordsStats(char *path, int clientID) {
+    char fileName[500];
+    sprintf(fileName, "%s/%d.txt", path, clientID);
+    FILE *fp = fopen(fileName, "r");
+    if (fp == NULL) return 1;
+    char readbuf[1000];
+    while (getLineFromFile(fp, readbuf, sizeof(readbuf)) > 0) {
+        char *token = strtok(readbuf, "\n");
+        token = strtok(token, " ");
+        while (token != NULL) {
+            wordsLength[strlen(token) - 1]++;
+            token = strtok(NULL, " ");
+        }
+    }
+    is_empty_array = 0;
+
+    return 0;
 }
 
-
 int main(int argc, char *argv[]) {
-
     // process input arguments
-	if (argc != NUM_ARGS) {
-		printf("Wrong number of args, expected %d", NUM_ARGS);
-		exit(1);
-	}
-	int i;
-	char folder_name[200];
-	strcpy(folder_name, argv[1]);
-	int clients_num = atoi(argv[2]);
-	char server_IP[200];
-	strcpy(server_IP, argv[3]);
-	short server_port = atoi(argv[4]);
-	char log_path[400];
-	strcpy(log_path, "log/log_client.txt");
-	logfp = fopen(log_path, "r");
-	
-	
-	struct sockaddr_in address;
-	address.sin_family = AF_INET;
-	address.sin_port = htons(server_port);
-	address.sin_addr.s_addr = inet_addr(server_IP);
+    if (argc != NUM_ARGS) {
+        printf("Wrong number of args, expected %d", NUM_ARGS);
+        exit(1);
+    }
+    int i;
+    char folder_name[200];
+    strcpy(folder_name, argv[1]);
+    int clients_num = atoi(argv[2]);
+    char server_IP[200];
+    strcpy(server_IP, argv[3]);
+    short server_port = atoi(argv[4]);
+    char log_path[400];
+    strcpy(log_path, "log/log_client.txt");
+    logfp = fopen(log_path, "r");
+
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_port = htons(server_port);
+    address.sin_addr.s_addr = inet_addr(server_IP);
 
     // create log file
     createLogFile();
-    
+
     // spawn client processes
     pid_t client;
-    
-	for (i = 0; i < clients_num; i++){
-		if ( (client = fork()) == 0 ) {
-			int request_structure[23];
-			int client_ID = i+1;
-			int j;
 
-			//Client ID
-			request_structure[1] = i+1;
-			//Socket for UPDATE_WSTAT
-			int sockfd = socket(AF_INET , SOCK_STREAM , 0);
-			if (connect(sockfd, (struct sockaddr *) &address, sizeof(address)) == 0) {
-				fprintf(logfp, "[%d] open connection\n", client_ID);
-				//Get the words length stats		
-				getWordsStats(folder_name, client_ID);
-				if (is_empty_array == 0) {
-					request_structure[0] = 1;
-					request_structure[1] = client_ID;
-					for (j = 2; j < 23; j++) {
-						request_structure[j] = wordsLength[j-2];
-					}
-					write(sockfd, request_structure, sizeof(request_structure));
-				}
-				int read_buf[LONG_RESPONSE_MSG_SIZE];
-				read(sockfd, read_buf, sizeof(read_buf));
-				fprintf(logfp, "[%d] UPDATE_WSTAT: %d\n", client_ID, read_buf[2]);			
-				close(sockfd); 
-				fprintf(logfp, "[%d] close connection (successful execution)\n", client_ID);
-					
-			} else {
-				perror("Sockfd1 connection failed!");
-			}
-			
-			//Socket for GET_MY_UPDATES
-			int sockfd2 = socket(AF_INET , SOCK_STREAM , 0);
-			if (connect(sockfd2, (struct sockaddr *) &address, sizeof(address)) == 0) {	
-				fprintf(logfp, "[%d] open connection\n", client_ID);
-				//request structure set up
-				request_structure[0] = 2;
-				request_structure[1] = client_ID;
-				write(sockfd, request_structure, sizeof(request_structure));
-				//Read back message from socket
-				int read_buf[LONG_RESPONSE_MSG_SIZE];
-				read(sockfd, read_buf, sizeof(read_buf));
-				fprintf(logfp, "[%d] GET_MY_UPDATES: %d %d\n", client_ID, read_buf[1], read_buf[2]);
-				close(sockfd2); 
-				fprintf(logfp, "[%d] close connection (successful execution)\n", client_ID);
-			} else {
-				perror("Sockfd2 connection failed!");
-			}
-			
-			//Socket for GET_ALL_UPDATES
-			int sockfd3 = socket(AF_INET , SOCK_STREAM , 0);
-			if (connect(sockfd3, (struct sockaddr *) &address, sizeof(address)) == 0) {
-				fprintf(logfp, "[%d] open connection\n", client_ID);
-				//request structure set up
-				request_structure[0] = 3;
-				request_structure[1] = client_ID;
-				write(sockfd, request_structure, sizeof(request_structure));
-				//Read back message from socket
-				int read_buf[LONG_RESPONSE_MSG_SIZE];
-				read(sockfd, read_buf, sizeof(read_buf));			
-				fprintf(logfp, "[%d] GET_ALL_UPDATES: %d %d\n", client_ID, read_buf[1], read_buf[2]);
-				close(sockfd3); 
-				fprintf(logfp, "[%d] close connection (successful execution)\n", client_ID);
-			} else {
-				perror("Sockfd3 connection failed!");
-			}
-			
-			//Socket for GET_WSTAT
-			int sockfd4 = socket(AF_INET , SOCK_STREAM , 0);
-			if (connect(sockfd4, (struct sockaddr *) &address, sizeof(address)) == 0) {
-				fprintf(logfp, "[%d] open connection\n", client_ID);
-				//request structure set up
-				request_structure[0] = 4;
-				request_structure[1] = client_ID;
-				write(sockfd, request_structure, sizeof(request_structure));
-				//Read back message from socket
-				int read_buf[LONG_RESPONSE_MSG_SIZE];
-				read(sockfd, read_buf, sizeof(read_buf));
-				fprintf(logfp, "[%d] GET_WSTAT: %d", client_ID, read_buf[1]);
-				for (j = 0; j < 20; j++){
-					fprintf(logfp, " %d", read_buf[j+2]);
-				}
-				fprintf(logfp, "\n");
-				close(sockfd4); 
-				fprintf(logfp, "[%d] close connection (successful execution)\n", client_ID);
-			} else {
-				perror("Sockfd4 connection failed!");
-			}
-										
-			break;		
-		} 
-	}
+    for (i = 0; i < clients_num; i++) {
+        if ((client = fork()) == 0) {
+            int request_structure[23];
+            int client_ID = i + 1;
+            int j;
+
+            //Client ID
+            request_structure[1] = i + 1;
+            //Socket for UPDATE_WSTAT
+            int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+            if (connect(sockfd, (struct sockaddr *)&address, sizeof(address)) == 0) {
+                fprintf(logfp, "[%d] open connection\n", client_ID);
+                //Get the words length stats
+                getWordsStats(folder_name, client_ID);
+                if (is_empty_array == 0) {
+                    request_structure[0] = 1;
+                    request_structure[1] = client_ID;
+                    for (j = 2; j < 23; j++) {
+                        request_structure[j] = wordsLength[j - 2];
+                    }
+                    write(sockfd, request_structure, sizeof(request_structure));
+                }
+                int read_buf[LONG_RESPONSE_MSG_SIZE];
+                read(sockfd, read_buf, sizeof(read_buf));
+                fprintf(logfp, "[%d] UPDATE_WSTAT: %d\n", client_ID, read_buf[2]);
+                close(sockfd);
+                fprintf(logfp, "[%d] close connection (successful execution)\n", client_ID);
+
+            } else {
+                perror("Sockfd1 connection failed!");
+            }
+
+            //Socket for GET_MY_UPDATES
+            int sockfd2 = socket(AF_INET, SOCK_STREAM, 0);
+            if (connect(sockfd2, (struct sockaddr *)&address, sizeof(address)) == 0) {
+                fprintf(logfp, "[%d] open connection\n", client_ID);
+                //request structure set up
+                request_structure[0] = 2;
+                request_structure[1] = client_ID;
+                write(sockfd, request_structure, sizeof(request_structure));
+                //Read back message from socket
+                int read_buf[LONG_RESPONSE_MSG_SIZE];
+                read(sockfd, read_buf, sizeof(read_buf));
+                fprintf(logfp, "[%d] GET_MY_UPDATES: %d %d\n", client_ID, read_buf[1], read_buf[2]);
+                close(sockfd2);
+                fprintf(logfp, "[%d] close connection (successful execution)\n", client_ID);
+            } else {
+                perror("Sockfd2 connection failed!");
+            }
+
+            //Socket for GET_ALL_UPDATES
+            int sockfd3 = socket(AF_INET, SOCK_STREAM, 0);
+            if (connect(sockfd3, (struct sockaddr *)&address, sizeof(address)) == 0) {
+                fprintf(logfp, "[%d] open connection\n", client_ID);
+                //request structure set up
+                request_structure[0] = 3;
+                request_structure[1] = client_ID;
+                write(sockfd, request_structure, sizeof(request_structure));
+                //Read back message from socket
+                int read_buf[LONG_RESPONSE_MSG_SIZE];
+                read(sockfd, read_buf, sizeof(read_buf));
+                fprintf(logfp, "[%d] GET_ALL_UPDATES: %d %d\n", client_ID, read_buf[1], read_buf[2]);
+                close(sockfd3);
+                fprintf(logfp, "[%d] close connection (successful execution)\n", client_ID);
+            } else {
+                perror("Sockfd3 connection failed!");
+            }
+
+            //Socket for GET_WSTAT
+            int sockfd4 = socket(AF_INET, SOCK_STREAM, 0);
+            if (connect(sockfd4, (struct sockaddr *)&address, sizeof(address)) == 0) {
+                fprintf(logfp, "[%d] open connection\n", client_ID);
+                //request structure set up
+                request_structure[0] = 4;
+                request_structure[1] = client_ID;
+                write(sockfd, request_structure, sizeof(request_structure));
+                //Read back message from socket
+                int read_buf[LONG_RESPONSE_MSG_SIZE];
+                read(sockfd, read_buf, sizeof(read_buf));
+                fprintf(logfp, "[%d] GET_WSTAT: %d", client_ID, read_buf[1]);
+                for (j = 0; j < 20; j++) {
+                    fprintf(logfp, " %d", read_buf[j + 2]);
+                }
+                fprintf(logfp, "\n");
+                close(sockfd4);
+                fprintf(logfp, "[%d] close connection (successful execution)\n", client_ID);
+            } else {
+                perror("Sockfd4 connection failed!");
+            }
+
+            break;
+        }
+    }
 
     // wait for all client processes to terminate
-	while(wait(NULL) > 0);
+    while (wait(NULL) > 0)
+        ;
 
     // close log file
     fclose(logfp);
 
     return EXIT_SUCCESS;
-
 }
--- project4/PA4_Server/src/server.c
+++ project4/PA4_Server/src/server.c
@@ -1,15 +1,16 @@
-#include <stdio.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <zconf.h>
-#include <pthread.h>
-#include <signal.h>
-#include <arpa/inet.h>
 #include <sys/wait.h>
+#include <zconf.h>
+
 #include "../include/protocol.h"
 
 #define NUM_ARGS 2
@@ -23,164 +24,159 @@ int empty_data[20];
 
 //Creating a struct for thread arguments
 struct thread_args {
-		int clientfd;
-		int client_port;	
-		char* client_ip;
+    int clientfd;
+    int client_port;
+    char *client_ip;
 };
 
-
-void update_wstat(int* request_structure) {
-	int i;
-	for (i = 0; i < 20; i++) {	
-		words_length[i] += request_structure[i+2];
-		//printf("index:%d, value:%d\n", i, words_length[i]);
-	}
+void update_wstat(int *request_structure) {
+    int i;
+    for (i = 0; i < 20; i++) {
+        words_length[i] += request_structure[i + 2];
+        //printf("index:%d, value:%d\n", i, words_length[i]);
+    }
 }
 
-int request_count_sum(){
-	int i, sum;
-	if (pthread_mutex_lock(&mutex)) {
-		fprintf(stderr, "failed to lock GET_ALL_UPDATES");
-		pthread_exit((int *) 1);
-	}
-	for (i = 0; i < 20; i++) {
-		sum += request_count[i];
-	}
-	if (pthread_mutex_unlock(&mutex)) {
-		fprintf(stderr, "failed to unlock GET_ALL_UPDATES");
-		pthread_exit((int *) 1);
-	}	
-	return sum;
+int request_count_sum() {
+    int i, sum;
+    if (pthread_mutex_lock(&mutex)) {
+        fprintf(stderr, "failed to lock GET_ALL_UPDATES");
+        pthread_exit((int *)1);
+    }
+    for (i = 0; i < 20; i++) {
+        sum += request_count[i];
+    }
+    if (pthread_mutex_unlock(&mutex)) {
+        fprintf(stderr, "failed to unlock GET_ALL_UPDATES");
+        pthread_exit((int *)1);
+    }
+    return sum;
 }
 
-void * socket_thread(void *arg) {
-	struct thread_args *new_arg = (struct thread_args*)arg;
-	int new_socket = new_arg -> clientfd;
-	char* client_ip = new_arg -> client_ip;
-	int client_port = new_arg -> client_port;
-	int recv_buf[REQUEST_MSG_SIZE];
-	
-	if (read(new_socket, recv_buf, sizeof(recv_buf)) > 0) {
-		//printf("recv_buf: %d\n", recv_buf[1]);
-		size_t recv_buf_length = sizeof(recv_buf)/sizeof(recv_buf[0]);
-		int client_ID = recv_buf[1];
-		if (recv_buf_length == 23 || client_ID < 0) {
-			if (recv_buf[0] == UPDATE_WSTAT ) {
-				printf("[%d] UPDATE_WSTAT\n", client_ID);
-				if (pthread_mutex_lock(&mutex)) {
-					fprintf(stderr, "failed to lock update_wstat");
-					pthread_exit((int *) 1);
-				}
-				update_wstat(recv_buf);
-				request_count[client_ID-1]++;
-				if (pthread_mutex_unlock(&mutex)) {
-					fprintf(stderr, "failed to unlock update_wstat");
-					pthread_exit((int *) 1);
-				}
-				int update_log[3];
-				update_log[0] = UPDATE_WSTAT;
-				update_log[1] = RSP_OK;
-				update_log[2] = 1;
-				write(new_socket, update_log, sizeof(update_log));
-			} else if (recv_buf[0] == GET_MY_UPDATES) {
-				printf("[%d] GET_MY_UPDATES\n", client_ID);	
-				if (pthread_mutex_lock(&mutex)) {
-					fprintf(stderr, "failed to lock GET_MY_UPDATES");
-					pthread_exit((int *) 1);
-				}
-				int count = request_count[client_ID-1];
-				int get_my_updates[3] = {GET_MY_UPDATES, RSP_OK, count};
-				write(new_socket, get_my_updates, sizeof(get_my_updates)); 
-				if (pthread_mutex_unlock(&mutex)) {
-					fprintf(stderr, "failed to unlock GET_MY_UPDATES");
-					pthread_exit((int *) 1);
-				}
-			} else if (recv_buf[0] == GET_ALL_UPDATES) {
-				printf("[%d] GET_ALL_UPDATES\n", client_ID);
-				int count_sum = request_count_sum(); 		
-				int get_all_data[3] = {GET_ALL_UPDATES, RSP_OK, count_sum};
-				write(new_socket, get_all_data, sizeof(get_all_data));
-			} else if (recv_buf[0] == GET_WSTAT) {
-				printf("[%d] GET_WSTAT\n", client_ID);
-				int get_wstat_reponse[22];
-				get_wstat_reponse[0] = GET_WSTAT;
-				get_wstat_reponse[1] = RSP_OK;
-				int z;
-				for (z = 0; z < 20; z++) {
-					get_wstat_reponse[z+2] = words_length[z];
-				}
-				write(new_socket, get_wstat_reponse, sizeof(get_wstat_reponse));
-			} else {
-				printf("Invalid request\n");
-				write(new_socket, empty_data, sizeof(empty_data));
-			}
-		} else {
-			printf("Invalid request\n");
-			write(new_socket, empty_data, sizeof(empty_data));
-		}
-		
-	}
-	printf("close connection from %s:%d\n", client_ip, client_port);
-	close(new_socket);	
-	return NULL;	
+void *socket_thread(void *arg) {
+    struct thread_args *new_arg = (struct thread_args *)arg;
+    int new_socket = new_arg->clientfd;
+    char *client_ip = new_arg->client_ip;
+    int client_port = new_arg->client_port;
+    int recv_buf[REQUEST_MSG_SIZE];
+
+    if (read(new_socket, recv_buf, sizeof(recv_buf)) > 0) {
+        //printf("recv_buf: %d\n", recv_buf[1]);
+        size_t recv_buf_length = sizeof(recv_buf) / sizeof(recv_buf[0]);
+        int client_ID = recv_buf[1];
+        if (recv_buf_length == 23 || client_ID < 0) {
+            if (recv_buf[0] == UPDATE_WSTAT) {
+                printf("[%d] UPDATE_WSTAT\n", client_ID);
+                if (pthread_mutex_lock(&mutex)) {
+                    fprintf(stderr, "failed to lock update_wstat");
+                    pthread_exit((int *)1);
+                }
+                update_wstat(recv_buf);
+                request_count[client_ID - 1]++;
+                if (pthread_mutex_unlock(&mutex)) {
+                    fprintf(stderr, "failed to unlock update_wstat");
+                    pthread_exit((int *)1);
+                }
+                int update_log[3];
+                update_log[0] = UPDATE_WSTAT;
+                update_log[1] = RSP_OK;
+                update_log[2] = 1;
+                write(new_socket, update_log, sizeof(update_log));
+            } else if (recv_buf[0] == GET_MY_UPDATES) {
+                printf("[%d] GET_MY_UPDATES\n", client_ID);
+                if (pthread_mutex_lock(&mutex)) {
+                    fprintf(stderr, "failed to lock GET_MY_UPDATES");
+                    pthread_exit((int *)1);
+                }
+                int count = request_count[client_ID - 1];
+                int get_my_updates[3] = {GET_MY_UPDATES, RSP_OK, count};
+                write(new_socket, get_my_updates, sizeof(get_my_updates));
+                if (pthread_mutex_unlock(&mutex)) {
+                    fprintf(stderr, "failed to unlock GET_MY_UPDATES");
+                    pthread_exit((int *)1);
+                }
+            } else if (recv_buf[0] == GET_ALL_UPDATES) {
+                printf("[%d] GET_ALL_UPDATES\n", client_ID);
+                int count_sum = request_count_sum();
+                int get_all_data[3] = {GET_ALL_UPDATES, RSP_OK, count_sum};
+                write(new_socket, get_all_data, sizeof(get_all_data));
+            } else if (recv_buf[0] == GET_WSTAT) {
+                printf("[%d] GET_WSTAT\n", client_ID);
+                int get_wstat_reponse[22];
+                get_wstat_reponse[0] = GET_WSTAT;
+                get_wstat_reponse[1] = RSP_OK;
+                int z;
+                for (z = 0; z < 20; z++) {
+                    get_wstat_reponse[z + 2] = words_length[z];
+                }
+                write(new_socket, get_wstat_reponse, sizeof(get_wstat_reponse));
+            } else {
+                printf("Invalid request\n");
+                write(new_socket, empty_data, sizeof(empty_data));
+            }
+        } else {
+            printf("Invalid request\n");
+            write(new_socket, empty_data, sizeof(empty_data));
+        }
+    }
+    printf("close connection from %s:%d\n", client_ip, client_port);
+    close(new_socket);
+    return NULL;
 }
 
 int main(int argc, char *argv[]) {
-
     // process input arguments
-	if (argc != NUM_ARGS) {
-		printf("Wrong number of args, expected %d\n", NUM_ARGS);
-		exit(1);
-	}
-	//init mutex lock
-	if (pthread_mutex_init(&mutex, NULL)) {
-		fprintf(stderr, "Failed to init mutex lock\n");
-		return 1;
-	}
-	
-    // socket create and verification 
-	int sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (argc != NUM_ARGS) {
+        printf("Wrong number of args, expected %d\n", NUM_ARGS);
+        exit(1);
+    }
+    //init mutex lock
+    if (pthread_mutex_init(&mutex, NULL)) {
+        fprintf(stderr, "Failed to init mutex lock\n");
+        return 1;
+    }
 
-    // assign IP, PORT 
-	short server_port = atoi(argv[1]);
-	int IP = INADDR_ANY;
-	
-	struct sockaddr_in servAddress;
-	servAddress.sin_family = AF_INET;
-	servAddress.sin_port = htons(server_port);
-	servAddress.sin_addr.s_addr = htonl(IP);
-	
+    // socket create and verification
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    // assign IP, PORT
+    short server_port = atoi(argv[1]);
+    int IP = INADDR_ANY;
+
+    struct sockaddr_in servAddress;
+    servAddress.sin_family = AF_INET;
+    servAddress.sin_port = htons(server_port);
+    servAddress.sin_addr.s_addr = htonl(IP);
+
     // Binding newly created socket to given IP
-	if ( bind(sock, (struct sockaddr *) &servAddress, sizeof(servAddress)) < 0){
-		fprintf(stderr, "Failed to bind socket thread.\n");	
-	}
-	
+    if (bind(sock, (struct sockaddr *)&servAddress, sizeof(servAddress)) < 0) {
+        fprintf(stderr, "Failed to bind socket thread.\n");
+    }
+
     // Now server is ready to listen
     listen(sock, MAX_CONNECTIONS);
-	printf("server is listening\n");
-	
-    while (1) {
+    printf("server is listening\n");
 
+    while (1) {
         // Accept the data packet from client
         struct sockaddr_in clientAddress;
-		pthread_t new_thread;
-		socklen_t size = sizeof(struct sockaddr_in);
-		struct thread_args *thread_arg;
-		thread_arg = malloc (sizeof(struct thread_args));
-		if ( (thread_arg->clientfd = accept(sock, (struct sockaddr*) &clientAddress, &size)) < 0 ) {
-			fprintf(stderr, "Failed to accpet socket.\n");
-		}
-		thread_arg->client_ip = inet_ntoa(clientAddress.sin_addr);
-		thread_arg->client_port = clientAddress.sin_port;
-		//printf("%s\n", clientIpAddr);
-		printf("open connection from %s:%d\n", thread_arg->client_ip, thread_arg->client_port);
-		
-		if ( pthread_create(&new_thread, NULL, socket_thread, thread_arg) != 0 ) {
-			fprintf(stderr, "Failed to create socket thread.\n");
-		}
-		
-		pthread_detach(new_thread);
-		
+        pthread_t new_thread;
+        socklen_t size = sizeof(struct sockaddr_in);
+        struct thread_args *thread_arg;
+        thread_arg = malloc(sizeof(struct thread_args));
+        if ((thread_arg->clientfd = accept(sock, (struct sockaddr *)&clientAddress, &size)) < 0) {
+            fprintf(stderr, "Failed to accpet socket.\n");
+        }
+        thread_arg->client_ip = inet_ntoa(clientAddress.sin_addr);
+        thread_arg->client_port = clientAddress.sin_port;
+        //printf("%s\n", clientIpAddr);
+        printf("open connection from %s:%d\n", thread_arg->client_ip, thread_arg->client_port);
+
+        if (pthread_create(&new_thread, NULL, socket_thread, thread_arg) != 0) {
+            fprintf(stderr, "Failed to create socket thread.\n");
+        }
+
+        pthread_detach(new_thread);
     }
     exit(EXIT_SUCCESS);
-} 
+}
